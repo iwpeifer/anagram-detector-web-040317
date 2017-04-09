209795990df0fae9598a5535931460e09836e6fe
iwpeifer <iwpeifer@gmail.com>
2017-04-09 18:00:05 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+	attr_accessor :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	def match(list)
+		possible_answers = []
+		sorted_list = list.map{|word| word.split("").sort.join("")}
+		sorted_word = self.word.split("").sort.join("")
+		sorted_list.each_with_index do |list_word, index|
+			if list_word == sorted_word
+				possible_answers << list[index]
+			end
+		end
+		possible_answers
+	end
+	
+end
